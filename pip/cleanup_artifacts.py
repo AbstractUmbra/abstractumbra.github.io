d8756db7a4964e1cd161004229f795e2c0cee508
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-12-05 18:50:15 +0000

    add debugging to task

--- pip/cleanup_artifacts.py
+++ pip/cleanup_artifacts.py
@@ -1,6 +1,8 @@
 import pathlib
 
 CURRENT_DIR = pathlib.Path(__file__).parent
+print(CURRENT_DIR)
+print("\n".join(file.name for file in CURRENT_DIR.iterdir()))
 
 for file in CURRENT_DIR.glob("*.whl"):
     package_name = file.name.split("-")[0]  # e.g. yarl-1.5.1 == yarl
