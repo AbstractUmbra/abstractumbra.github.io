d1c02ec6b5ce37f96d821388b253be5bf8d2b772
Alex Nørgaard <Umbra@AbstractUmbra.dev>
2022-12-05 18:26:55 +0000

    fix the artifact generation script

--- pip/cleanup_artifacts.py
+++ pip/cleanup_artifacts.py
@@ -4,7 +4,12 @@ CURRENT_DIR = pathlib.Path(__file__).parent
 
 for file in CURRENT_DIR.glob("*.whl"):
     package_name = file.name.split("-")[0]  # e.g. yarl-1.5.1 == yarl
+    print(f"Found {package_name}, preparing to move.")
     move_to = CURRENT_DIR / package_name
     if not move_to.exists():
-        move_to.mkdir(mode=755, exist_ok=True)
-    file.rename(move_to / file.name)
+        print(f"No directory for {move_to} found, creating it now.")
+        move_to.absolute().mkdir(mode=0o755, exist_ok=True)
+
+    new_path = move_to / file.name
+    print(f"Moving file to {new_path}")
+    file.replace(new_path)
