92b54120619f2a409b130d639c624bd2a61fe0ab
Alex Nørgaard <umbra@abstractumbra.dev>
2025-10-14 22:05:11 +0100

    also cleanup wheel filenames

--- pip/cleanup_artifacts.py
+++ pip/cleanup_artifacts.py
@@ -5,11 +5,18 @@ CURRENT_DIR = pathlib.Path(__file__).parent
 SPECIAL_CASES: dict[str, str] = {"discord_py": "discord.py", "lru_dict": "lru-dict"}
 
 
+def resolve_filename(name: str) -> str:
+    split_name = name.split("-")[0]  # e.g. yarl-1.5.1 == yarl
+    resolved = SPECIAL_CASES.get(split_name)
+    if not resolved:
+        return name
+
+    return name.replace(split_name, resolved, count=1)
+
+
 for file in CURRENT_DIR.glob("*.whl"):
     package_name = file.name.split("-")[0]  # e.g. yarl-1.5.1 == yarl
-    special_resolved_name = SPECIAL_CASES.get(package_name)
-    if special_resolved_name:
-        package_name = special_resolved_name
+    package_name = SPECIAL_CASES.get(package_name, package_name)
 
     print(f"Found {package_name}, preparing to move.")
     move_to = CURRENT_DIR / package_name
@@ -17,7 +24,8 @@ for file in CURRENT_DIR.glob("*.whl"):
         print(f"No directory for {move_to} found, creating it now.")
         move_to.absolute().mkdir(mode=0o755, exist_ok=True)
 
-    new_path = move_to / file.name
+    filename = resolve_filename(file.name)
+    new_path = move_to / filename
     print(f"Moving file to {new_path}")
     file.replace(new_path)
 else:
